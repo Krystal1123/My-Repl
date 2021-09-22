d536b6f5fa66621d1bc03973c5871bf6f7a9ff05
Krystal1123 <91225311+Krystal1123@users.noreply.github.com>
2021-09-22 21:49:52 +0000

    Added code

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-print("Hello world")
-print("Hello")
-print("Hi")
+import random
+lowerNumber = 5
+upperNumber = 30
+randomNumber = random.randint(lowerNumber, upperNumber)
+
+print("Welcome to my Random Number Guessing Game")
+playerguess = int(input("Pick a number between 5 and 30"))
+
+if playerguess == randomNumber:
+  print("You win!")
+else:
+  print(randomNumber)
+  print("You lose")
